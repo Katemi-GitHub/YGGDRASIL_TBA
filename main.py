80e53099a36cbce43160acefa560b43ce0504393
Katemi Dev <60159484+Katemi-GitHub@users.noreply.github.com>
2023-02-15 00:11:47 +0000

    Update main.py
    Whole game engine changed

--- main.py
+++ main.py
@@ -1,102 +1,89 @@
-# Project started 12/2/23
+# project started 12/02/2023
 
-import pygame
+import tkinter as tk
 import json
-from pygame.locals import *
+import time
 
-with open("settings.json", "r") as f:
-    settings = json.load(f)
+# load and read player save and settings
+with open("settings.json", "r") as file:
+    settings = json.load(file)
 
-with open("save.json", "r") as f:
-    save = json.load(f)
+with open("save.json", "r") as file:
+    save = json.load(file)
 
-pygame.init()
-window = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("YGGDRASIL || BUILD 0.1DEV")
-font = pygame.font.Font(None, 20)
+# window initializer
 
-entry_text = ""
-entry_text_render = font.render("> " + entry_text, True, (255, 255, 255))
-chat = []
-scroll = 0
-tags = ""
+version = "Alpha-0.1" # version of the project
+name = "YGGDRASIL"
+sizeWnd = "800x600" # window size
+frame = tk.Tk()
+frame.title(name) # window name display
+frame.geometry(sizeWnd)
 
-def TBA_print(type, message):
-    if type == 1:
-        end_message = "[SYSTEM] " + message
-    message_text = font.render(end_message, True, (255, 255, 255))
-    text_width = message_text.get_rect().width
-    lines = text_width // 75 + 1
-    for i in range(lines):
-        line = end_message[i * 75:(i + 1) * 75]
-        chat.append(line)
-    for i, text in enumerate(chat):
-        if text == "":
-            chat.pop(i)
+# game var outside the "settings.json" file
 
-TBA_print(1, "Wellcome to YGGDRASIL BETA 1.0")
+chat_history = [] # stores the text displayed on the window
+user_last_input = tk.StringVar()
+start_time = time.time() # initialization time
+end_time = 0
+
+# TODO:
+# 1- Icon for the window
+# 2- Crafting system
+# 3- Map and map syntax
+# 3.5- Goto function
+
+# there goes the functions used through the game
+
+def tgaPrint(msgType=int, msg=str):
+    chat_history.append(msg)
+    typeTag = "" # typeTag defines which kind of message will be sent
+    if msgType == 0: # type 0 for system messages
+        typeTag = "SYSTEM"
+    if msgType == 1: # type 1 for world notifications
+        typeTag = "WORLD"
+    text_area.insert(tk.END, "["+typeTag+"] "+msg+"\n") # print into the text_area the desired message
+
+def append_message(Event=None):
+    global user_last_input
+    user_last_input.set(text_input.get())
+    text_input.delete(0, tk.END)
+
+def saveFiles():
+    with open("settings.json", "w") as file:
+        json.dump(settings, file)
+
+    with open("save.json", "w") as file:
+        json.dump(save, file)
+
+# there goes the GUI elements
+
+bottom_bar = tk.Frame(frame, bg="grey90", height=20)
+bottom_bar.pack(side="bottom", fill="x")
+
+version_display = tk.Label(bottom_bar, text=version)
+version_display.pack(side=tk.LEFT)
+
+text_input = tk.Entry(frame)
+text_input.config(bd=1, relief="raised")
+text_input.pack(side=tk.BOTTOM, fill=tk.X)
+text_input.bind("<Return>", append_message)
+
+text_area = tk.Text(frame, state="normal")
+text_area.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+text_area.config(bg="black", fg="white", bd=1, relief="sunken")
+
+# there goes the game logic (NOT FUNCTIONS)
+
+tgaPrint(0, "Wellcome to YGGDRASIL")
 
 if settings["first_time"] == "True":
-    TBA_print(1, "Please enter your name")
-else:
-    TBA_print(1, "Wellcome " + save["name"])
+    tgaPrint(0, "Please adventurer, type your name")
+    frame.update()
+    frame.wait_variable(user_last_input)
+    save["name"] = user_last_input.get()
+    tgaPrint(0, "Be wellcome "+save["name"]+" to Yggdrasil, the almighty tree of life and creation!")
+    settings["first_time"] = "False"
+    saveFiles()
 
-def game_logic(list, message):
-    if settings["first_time"]:
-        if save["name"] == "":
-            save["name"] = message
-            save["tags"].append(message)
-            settings["first_time"] = "False"
-            with open("save.json", "w") as f:
-                json.dump(save, f)
-            with open("settings.json", "w") as f:
-                json.dump(settings, f)
-            TBA_print(1, "Wellcome " + save["name"])
-    if message == "":
-        return
-
-running = True
-while running:
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            if event.unicode.isalnum() or event.unicode == " ":
-                entry_text += event.unicode
-            elif event.key == pygame.K_BACKSPACE:
-                entry_text = entry_text[:-1]
-            elif event.key == K_RETURN:
-                if scroll <= len(chat)*20:
-                    scroll == len(chat)*20
-                if entry_text != "" and len(chat)*20 > 520:
-                    scroll += 20
-                for tag in save["tags"]:
-                    tags = "[" + tag + "]"
-                chat_text = font.render(entry_text, True, (255, 255, 255))
-                text_width = chat_text.get_rect().width
-                lines = text_width // 75 + 1
-                for i in range(lines):
-                    line = tags + " " + entry_text[i * 75:(i + 1) * 75]
-                    chat.append(line)
-                game_logic(chat, entry_text)
-                entry_text = ""
-            entry_text_render = font.render("> " + entry_text, True, (255, 255, 255))
-        if event.type == MOUSEWHEEL:
-            if event.y == -1:
-                if scroll <= (len(chat)*20 - 600):
-                    scroll += 20
-            elif event.y == 1:
-                if scroll >= 20:
-                    scroll -= 20
-
-    window.fill((0, 0, 0))
-    pygame.draw.rect(window, (55, 55, 55), (0, 540, 800, 60), width=5)
-    window.blit(entry_text_render, (10, 550))
-    for i, text in enumerate(chat):
-        if text == "":
-            chat.pop(i)
-    for i, text in enumerate(chat):
-        chat_text = font.render(text, True, (255, 255, 255))
-        window.blit(chat_text, (10, 20*(i+1) - scroll))
-    pygame.display.update()
+frame.mainloop()
